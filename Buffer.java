64df5ce5ac6ea847a820c2252e1a24ac69f2ae8a
anushyaa7 <116780962+anushyaa7@users.noreply.github.com>
2022-11-15 19:54:07 +0530

    Add files via upload

---
+++ Buffer.java
@@ -0,0 +1,21 @@
+package com.spillted;
+
+public class Buffer {
+	public static void main(String[] args) {
+		String s="Anushya";
+		StringBuffer sb=new StringBuffer(s);
+		System.out.println(sb);
+		System.out.println(sb.toString());
+		System.out.println(sb.insert(3, "R"));//added
+		System.out.println(sb.capacity());
+		System.out.println(sb.toString());//display repeated
+	}
+	 class Buffer1  {
+		public static String m1(String s) {
+			StringBuffer sb=new StringBuffer(s);
+			System.out.println(sb.insert(5, "X"));
+			System.out.print(sb.toString());
+			return sb.toString(); }
+		public static void main(String[] args) {
+			m1("Anushya");		
+		}}}
